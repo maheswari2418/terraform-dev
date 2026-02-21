9622b9638d3f6eede07a7eb78e700b9005ef7c2b
maheswari2418 <maheswari1187@gmail.com>
2026-02-21 13:03:36 +0530

    Updated

--- main.tf
+++ main.tf
@@ -62,3 +62,47 @@ resource "aws_route_table_association" "private_rt" {
     route_table_id = aws_route_table.private_rt.id
   
 }
+resource "aws_security_group" "public-sg" {
+    name="public-sg"
+    description = "Allow SSh and HTTP"
+    vpc_id=aws_vpc.main.id
+    ingress{
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+        from_port=80
+        to_port=80
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        from_port="0"
+        to_port="0"
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    tags={
+        Name="public-sg"
+    }
+  
+}
+resource "aws_security_group" "private-sg"{
+    name="private-sg"
+    description="Allow only SSH from public-sg"
+    vpc_id=aws_vpc.main.id
+    ingress{
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        security_groups=[aws_security_group.public-sg.id]
+    }
+    egress{
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
